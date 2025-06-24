2948e2da3627a2bfa0f49f467746d1b4fbcaa62b
Dinkar Dubey <sde.dinkardubey@gmail.com>
2025-06-25 03:37:25 +0530

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Purgata',
+  description: 'Purgata - A movie review sentiment analysis tool.',
+  generator: 'Developed by Dinkar for Purgata',
 }
 
 export default function RootLayout({
